58de8c5feeb2b13bc4df3b93cfc52c08e060ae57
rakibtg <rakibtg@gmail.com>
2018-07-16 00:03:06 +0600

    user model

---
+++ src/user/user.model.js
@@ -0,0 +1,48 @@
+import mongoose from 'mongoose'
+
+const UserSchema = mongoose.Schema({
+  name: {
+      type        : String,
+      required    : true,
+      index       : true
+  },
+  userName: {
+      type        : String,
+      required    : true,
+      unique      : true,
+      index       : true,
+  },
+  email: {
+      type        : String,
+      required    : true,
+      unique      : true,
+      index       : true,
+  },
+  createdAt: {
+      type        : Date,
+      default     : Date.now,
+  }
+}, {
+  collection: 'User'
+})
+
+let UsersModel = mongoose.model( 'User', UserSchema )
+
+// Get all users.
+UsersModel.getAll = () => (
+  UsersModel.find({})
+)
+
+// Add a new usre.
+UsersModel.addUser = user => (
+  UsersModel( user )
+    .save()
+)
+
+// Delete a user.
+UsersModel.removeUser = userName => (
+  UsersModel.remove({ userName: userName })
+)
+
+
+export default UsersModel
