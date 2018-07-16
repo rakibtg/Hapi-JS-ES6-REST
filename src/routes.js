6b59ae56db0febfca6fe3cefcd1e1d8c18e077d1
Kazi Mehedi Hasan <rakibtg@gmail.com>
2018-07-16 13:32:46 +0600

    authSignUpRoutes and authSignInRoutes

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,13 @@
-import { defaultRoutes } from './default/default.routes'
-import { userRoutes } from './user/user.routes'
+import { defaultRoutes }      from './default/default.routes'
+import { userRoutes }         from './user/user.routes'
+import { authSignUpRoutes }   from './auth/signup/signup.routes'
+import { authSignInRoutes }   from './auth/signin/signin.routes'
 
 const routes = [
   defaultRoutes,
   userRoutes,
+  authSignUpRoutes,
+  authSignInRoutes,
 ]
 
 export default routes
